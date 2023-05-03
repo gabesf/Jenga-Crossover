eedf5a15ccf8124e9d11c7fe7ee082fec4a8da17
gabesf <gabriel.sffair@refract.gg>
2023-05-03 05:54:41 -0300

    starting UI Controller

---
+++ Assets/UiController.cs
@@ -0,0 +1,12 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiController : MonoBehaviour
+{
+    public Button testStackButton;
+    public Button earthQuakeButton;
+    public Button nextStackButton;
+    public Button previousStackButton;
+
+}
